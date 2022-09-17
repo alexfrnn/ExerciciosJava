9de09148ee912113958392535122ecb1aacbe557
Alexfrnn <alexfernand.o@outlook.com>
2022-09-17 10:13:14 -0300

    Question 08

---
+++ Question08.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Question08 {
+    public static void main(String[] args) {
+        double rendaConjunta = 0, ALIO, rendaHomem, rendaMulher;
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Digite a renda do homem");
+        rendaHomem = input.nextDouble();
+
+        System.out.println("Digite a renda da mulher");
+        rendaMulher = input.nextDouble();
+
+        rendaConjunta = (rendaHomem + rendaMulher);
+
+        System.out.println("");
+    }
+}
