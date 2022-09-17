6e8b9fd666875221b4383e0cbb612219671252f2
Alexfrnn <alexfernand.o@outlook.com>
2022-09-17 10:10:36 -0300

    Question 07

---
+++ Question07.java
@@ -0,0 +1,23 @@
+package java_lessons;
+
+import java.util.Scanner;
+
+public class Question07 {
+	public static void main(String[] args) {
+		int cedulasInseridas;
+		
+		int notasDeCem;
+		//notasDeCinquenta, notasDeVinte, notasDeDez, notasDeCinco, notasDeDois, umReal;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Digite a quantidade de cédulas");
+		cedulasInseridas = input.nextInt();
+		
+		notasDeCem = cedulasInseridas / 100;
+		
+		System.out.println("valor lido "+ cedulasInseridas);
+		System.out.println(notasDeCem + " notas de R$100");
+	input.close();
+	
+	}
+	
+}
