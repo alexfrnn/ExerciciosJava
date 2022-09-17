cf4a09494e030d276ecdf8c73681c57c227071d5
Alexfrnn <alexfernand.o@outlook.com>
2022-09-17 10:17:20 -0300

    Question 08

--- Question08.java
+++ Question08.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 
 public class Question08 {
     public static void main(String[] args) {
-        double rendaConjunta = 0, ALIO, rendaHomem, rendaMulher;
+        double rendaConjunta = 0;
+        double ALIO = 0;
+        double rendaHomem, rendaMulher;
         
         Scanner input = new Scanner(System.in);
         
