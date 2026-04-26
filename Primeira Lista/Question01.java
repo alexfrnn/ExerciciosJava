98d551c6b750f57f1156009cce3c002ebe4423dc
Alex F <93062390+alexfrnn@users.noreply.github.com>
2026-04-25 23:54:17 -0300

    Rename variable 'media' to 'average'

--- Primeira Lista/Question01.java
+++ Primeira Lista/Question01.java
@@ -11,9 +11,9 @@ public class Question01 {
         System.out.println("Digite a segunda nota: ");
         double secondGrade = input.nextDouble();
         
-        double media = ((firstGrade * 3.5 )+(secondGrade * 7.5)) / 11;
+        double average = ((firstGrade * 3.5 )+(secondGrade * 7.5)) / 11;
 
-        System.out.printf("A média é: %.2f%n", media);
+        System.out.printf("A média é: %.2f%n", average);
         input.close();
     }
 }
